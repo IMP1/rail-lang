6fe71a2f233a15fcbac17b9462bf0194156792c7
IMP1 <IMP1@mail.com>
2019-11-13 14:17:15 +0000

    Add base rails

---
+++ cells/junctions.rb
@@ -0,0 +1,80 @@
+def junction(cell, world, train, stack, direction)
+    unless directions.keys.include?(train.direction)
+        raise PerpendicularRailCrash.new(cell)
+    end
+    x, y = *train.position
+    if directions.values.flatten.uniq.any? { |dir| world.cell_to_direction(x, y, dir).empty? }
+        raise UnconnectedJunctionCrash.new(cell)
+    end
+    divert = stack.pop
+    outgoing_index = divert ? 1 : 0
+    train.turn(directions[train.direction][outgoing_index])
+end
+
+Cell.create("Junction", '<') do |cell, world, train, stack|
+    directions = {
+        # incoming => [left_outgoing, right_outgoing],
+        Direction::EAST => [Direction::NORTH_EAST, Direction::SOUTH_EAST],
+        Direction::SOUTH_WEST => [Direction::SOUTH_EAST, Direction::WEST],
+        Direction::NORTH_WEST => [Direction::WEST, Direction::NORTH_EAST],
+    }
+    junction(cell, world, train, stack, directions)
+end
+
+Cell.create("Junction", '>') do |cell, world, train, stack|
+    directions = {
+        # incoming => [left_outgoing, right_outgoing],
+        Direction::WEST => [Direction::SOUTH_WEST, Direction::NORTH_WEST],
+        Direction::SOUTH_EAST => [Direction::EAST, Direction::SOUTH_WEST],
+        Direction::NORTH_EAST => [Direction::NORTH_WEST, Direction::EAST],
+    }
+    junction(cell, world, train, stack, directions)
+end
+
+#  |
+#  ^
+# / \
+Cell.create("Junction", '^') do |cell, world, train, stack|
+    directions = {
+        # incoming => [left_outgoing, right_outgoing],
+        Direction::SOUTH => [Direction::SOUTH_EAST, Direction::SOUTH_WEST],
+        Direction::NORTH_EAST => [Direction::NORTH, Direction::SOUTH_EAST],
+        Direction::NORTH_WEST => [Direction::SOUTH_WEST, Direction::NORTH],
+    }
+    junction(cell, world, train, stack, directions)
+end
+
+# \ /
+#  v
+#  |
+Cell.create("Junction", 'v') do |cell, world, train, stack|
+    directions = {
+        # incoming => [left_outgoing, right_outgoing],
+        Direction::NORTH => [Direction::NORTH_WEST, Direction::NORTH_EAST],
+        Direction::SOUTH_EAST => [Direction::NORTH_EAST, Direction::SOUTH],
+        Direction::SOUTH_WEST => [Direction::SOUTH, Direction::NORTH_WEST],
+    }
+    junction(cell, world, train, stack, directions)
+end
+
+# Cell.create("Switch", '=') do |world, train, stack|
+#     raise CrashException.new("Perpendicular rail") if train.direction == Direction::NORTH
+#     raise CrashException.new("Perpendicular rail") if train.direction == Direction::SOUTH
+#     divert = stack.pop
+#     if train.direction & Direction::EAST != 0
+#         if divert
+#             # TODO: find track
+#         else
+#             train.turn(Direction::EAST)
+#         end
+#     end
+#     if train.direction & Direction::WEST != 0
+#         if divert
+#             # TODO: find track
+#         else
+#             train.turn(Direction::WEST)
+#         end 
+#     end
+# end
+ 
+
---
+++ cells/rails.rb
@@ -0,0 +1,71 @@
+def straight_rail(cell, world, train, primaries, perpendiculars)
+    if perpendiculars.include?(train.direction)
+        raise PerpendicularRailCrash.new(cell)
+    end
+    unless primaries.include?(train.direction)
+        primaries.each do |dir|
+            if train.direction & dir != 0
+                train.turn(dir)
+            end
+        end
+    end
+    x, y = *train.position
+    if world.cell_to_direction(x, y, train.direction).empty?
+        secondary_directions = Direction.secondary_directions(train.direction)
+        secondary_cells = secondary_directions.map { |dir| world.cell_to_direction(x, y, dir) }
+        if secondary_cells.all? { |cell| cell.empty? }
+            raise UnconnectedJunctionCrash.new(cell)
+        end
+        if secondary_cells.count { |cell| !cell.empty? } > 1
+            raise AmbiguousConnectionCrash.new(cell)
+        end
+        secondary_direction_index = secondary_cells.find_index { |cell| !cell.empty }
+        train.turn(secondary_directions[secondary_direction_index])
+    end
+end
+
+Cell.create("Rail", '-') do |cell, world, train, stack|
+    primaries = [Direction::EAST, Direction::WEST]
+    perpendiculars = [Direction::NORTH, Direction::SOUTH]
+    straight_rail(cell, world, train, primaries, perpendiculars)
+end
+
+Cell.create("Rail", '|') do |cell, world, train, stack|
+    primaries = [Direction::NORTH, Direction::SOUTH]
+    perpendiculars = [Direction::EAST, Direction::WEST]
+    straight_rail(cell, world, train, primaries, perpendiculars)
+end
+
+Cell.create("Rail", '\\') do |cell, world, train, stack|
+    primaries = [Direction::NORTH_WEST, Direction::SOUTH_EAST]
+    perpendiculars = [Direction::NORTH_EAST, Direction::SOUTH_WEST]
+    straight_rail(cell, world, train, primaries, perpendiculars)
+end
+
+Cell.create("Rail", '//') do |cell, world, train, stack|
+    primaries = [Direction::NORTH_EAST, Direction::SOUTH_WEST]
+    perpendiculars = [Direction::NORTH_WEST, Direction::SOUTH_EAST]
+    straight_rail(cell, world, train, primaries, perpendiculars)
+end
+
+Cell.create("Rail", 'x') do |cell, world, train, stack|
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::NORTH
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::EAST
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::SOUTH
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::WEST
+end
+
+Cell.create("Rail", '+') do |cell, world, train, stack|
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::NORTH_EAST
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::NORTH_WEST
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::SOUTH_EAST
+    raise PerpendicularRailCrash.new(cell) if train.direction == Direction::SOUTH_WEST
+end
+
+Cell.create("Rail", '*') do |cell, world, train, stack|
+    
+end
+
+Cell.create("Rail", '@') do |cell, world, train, stack|
+    train.turn(Direction.inverse(train.direction))
+end
