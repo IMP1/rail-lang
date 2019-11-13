9113b73efb9a87ff991c922ee20e8df21f156d11
IMP1 <IMP1@mail.com>
2019-11-13 16:08:36 +0000

    Provide default messages

--- exceptions.rb
+++ exceptions.rb
@@ -1,22 +1,39 @@
 class CrashException < Exception
 
-    attr_reader :cell
-    attr_reader :train
-    attr_reader :world
+    attr_reader :message
 
-    def initialize(cell=nil, train=nil, world=nil)
-        @cell = cell
-        @train = train
-        @world = world
+    def initialize(message="")
+        @message = message
     end
 
 end
 
 class PerpendicularRailCrash < CrashException
+    def initialize
+        super("Perpendicular rail")
+    end
 end
 
 class UnconnectedJunctionCrash < CrashException
+    def initialize
+        super("Unconnected junction")
+    end
 end
 
 class AmbiguousConnectionCrash < CrashException
+    def initialize
+        super("Ambiguous connetion")
+    end
+end
+
+class EmptySpaceCrash < CrashException
+    def initialize
+        super("Empty space")
+    end
+end
+
+class UnrecognisedSymbolCrash < CrashException
+    def initialize
+        super("Unrecognised symbol")
+    end
 end
