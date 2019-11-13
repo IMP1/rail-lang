41f216ff4570a12fb791ff0102c451e487f47cc4
IMP1 <IMP1@mail.com>
2019-11-13 17:00:45 +0000

    Check for correct branches, rather than any

--- cells/junctions.rb
+++ cells/junctions.rb
@@ -1,10 +1,29 @@
-def junction(cell, world, train, stack, directions)
+def junction(cell, world, train, stack, directions, branches)
     unless directions.keys.include?(train.direction)
-        raise PerpendicularRailCrash.new(cell)
+        raise PerpendicularRailCrash.new
     end
+    # TODO: make sure all branches are correct as well.
+    #       a junction not only must have all outgoing branches,
+    #       but all incoming branches as well.
     x, y = *train.position
     if directions.values.flatten.uniq.any? { |dir| world.cell_to_direction(x, y, dir).empty? }
-        raise UnconnectedJunctionCrash.new(cell)
+        raise UnconnectedJunctionCrash.new
+    end
+    if directions.values.flatten.uniq.any? { |dir| world.cell_to_direction(x, y, dir).empty? }
+        raise UnconnectedJunctionCrash.new
+    end
+    begin
+        start_dir = Direction.inverse(branches[0])
+        expected_glyphs = branches[2..-1]
+        if world.cell_to_direction(x, y, start_dir).glyph != branches[1]
+            raise UnconnectedJunctionCrash.new
+        end
+        missing_branches = directions[branches.first].each_with_index.select do |dir, i| 
+            world.cell_to_direction(x, y, dir).glyph != expected_glyphs[i] 
+        end
+        if missing_branches.any?
+            raise UnconnectedJunctionCrash.new
+        end
     end
     divert = stack.pop
     outgoing_index = divert ? 1 : 0
@@ -18,7 +37,9 @@ Cell.create("Junction", '<') do |cell, world, train, stack|
         Direction::SOUTH_WEST => [Direction::SOUTH_EAST, Direction::WEST],
         Direction::NORTH_WEST => [Direction::WEST, Direction::NORTH_EAST],
     }
-    junction(cell, world, train, stack, directions)
+    # incoming => [left_branch_glyph, right_branch_glyph]
+    branches = [Direction::EAST, "-", "/", "\\"]
+    junction(cell, world, train, stack, directions, branches)
 end
 
 Cell.create("Junction", '>') do |cell, world, train, stack|
@@ -28,7 +49,9 @@ Cell.create("Junction", '>') do |cell, world, train, stack|
         Direction::SOUTH_EAST => [Direction::EAST, Direction::SOUTH_WEST],
         Direction::NORTH_EAST => [Direction::NORTH_WEST, Direction::EAST],
     }
-    junction(cell, world, train, stack, directions)
+    # [incoming, left_branch_glyph, right_branch_glyph]
+    branches = [Direction::WEST, "-", "/", "\\"]
+    junction(cell, world, train, stack, directions, branches)
 end
 
 #  |
@@ -41,7 +64,9 @@ Cell.create("Junction", '^') do |cell, world, train, stack|
         Direction::NORTH_EAST => [Direction::NORTH, Direction::SOUTH_EAST],
         Direction::NORTH_WEST => [Direction::SOUTH_WEST, Direction::NORTH],
     }
-    junction(cell, world, train, stack, directions)
+    # incoming => [left_branch_glyph, right_branch_glyph]
+    branches = [Direction::SOUTH, "|", "\\", "/"]
+    junction(cell, world, train, stack, directions, branches)
 end
 
 # \ /
@@ -54,7 +79,9 @@ Cell.create("Junction", 'v') do |cell, world, train, stack|
         Direction::SOUTH_EAST => [Direction::NORTH_EAST, Direction::SOUTH],
         Direction::SOUTH_WEST => [Direction::SOUTH, Direction::NORTH_WEST],
     }
-    junction(cell, world, train, stack, directions)
+    # incoming => [left_branch_glyph, right_branch_glyph]
+    branches = [Direction::NORTH, "|", "\\", "/"]
+    junction(cell, world, train, stack, directions, branches)
 end
 
 # Cell.create("Switch", '=') do |world, train, stack|
