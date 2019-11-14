c443e271072ae506f608dace6220ecaaec9064f2
IMP1 <IMP1@mail.com>
2019-11-14 11:07:58 +0000

    Handle special characters and escapes in constants

--- cells/constant.rb
+++ cells/constant.rb
@@ -1,16 +1,30 @@
+ESCAPE_CHAR = "\\"
+
 def consume_constant(world, train, stack, end_char)
     constant_chars = []
     until train.over?(end_char, world)
         train.move(world, true)
-        constant_chars.push(world.cell_at(*train.position).glyph)
+        char = world.cell_at(*train.position).glyph
+        constant_chars.push(char)
+        if char == ESCAPE_CHAR
+            train.move(world, true)
+            escaped_chars = []
+            until train.over?(ESCAPE_CHAR, world)
+                escaped_char = world.cell_at(*train.position).glyph
+                constant_chars.push(escaped_char)
+                train.move(world, true)
+            end
+            constant_chars.push(ESCAPE_CHAR)
+        end
     end
+    constant_chars.pop # Remove closing bracket
     constant = constant_chars.join("")
-    case constant
-    when /\d+/
-        constant = constant.to_i
-    else
-        constant = constant.to_s
-    end
+    # Handle special characters
+    constant.gsub!(/\\\\/, "\\")
+    constant.gsub!(/\\\[\\/, "[")
+    constant.gsub!(/\\\]\\/, "]")
+    constant.gsub!(/\\n\\/, "\n")
+    constant.gsub!(/\\t\\/, "\t")
     stack.push(constant)
 end
 
