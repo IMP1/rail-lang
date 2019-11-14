62830d4bb36e99cac09537b68c8d963efea31aee
IMP1 <IMP1@mail.com>
2019-11-14 16:04:02 +0000

    Add tests for compactness

--- tests/constants/constant_compact.rb
+++ tests/constants/constant_compact.rb
@@ -6,11 +6,12 @@ Test.require do
 
 end
 
+
 test_run_1 = Test.run do
     source = '
 $ \'main\'
  \
-  --[1]-o-#
+  --[1]o#
 '
     runner = Runner.new(source)
     runner.run
@@ -19,20 +20,4 @@ end
 test_run_1.ensure do |result|
     assert(result.success)
     assert(result.output.chomp == "1")
-end
-
-
-test_run_2 = Test.run do
-    source = '
-$ \'main\'
- \
-  --]12[-o-#
-'
-    runner = Runner.new(source)
-    runner.run
-end
-
-test_run_2.ensure do |result|
-    assert(result.success)
-    assert(result.output.chomp == "12")
 end
---
+++ tests/functions/function_compact.rb
@@ -0,0 +1,27 @@
+require_relative '../test'
+
+Test.require do
+
+    require_relative '../../runner'
+
+end
+
+
+test_run_1 = Test.run do
+    source = '
+$ \'main\'
+ \
+  \-{other-function}o#
+
+$ \'other-function\'
+ \
+  \-[1]#
+'
+    runner = Runner.new(source)
+    runner.run
+end
+
+test_run_1.ensure do |result|
+    assert(result.success)
+    assert(result.output.chomp == "1")
+end
