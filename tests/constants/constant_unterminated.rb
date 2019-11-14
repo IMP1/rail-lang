5b79294aad14322bf9c3d685b520e40c123b8ed8
IMP1 <IMP1@mail.com>
2019-11-14 14:29:23 +0000

    Test for unterminated constant

---
+++ tests/constants/constant_unterminated.rb
@@ -0,0 +1,22 @@
+require_relative '../test'
+
+Test.require do
+
+    require_relative '../../runner'
+
+end
+
+test_run_1 = Test.run do
+    source = '
+$ \'main\'
+ \
+  --[Printin
+'
+    runner = Runner.new(source)
+    runner.run
+end
+
+test_run_1.ensure do |result|
+    assert(result.failure)
+    assert(result.error.is_a?(EmptySpaceCrash))
+end
