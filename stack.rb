5c2d5e1e2cc49ab2c57ec8986c11128bd23950d8
IMP1 <IMP1@mail.com>
2019-11-15 16:02:27 +0000

    Add size method accessor and debugging tool

--- stack.rb
+++ stack.rb
@@ -5,14 +5,24 @@ class Stack
     end
 
     def push(value)
+        if $debug
+            p @elements
+        end
         @elements.push(value)
     end
 
     def pop
+        if $debug
+            p @elements
+        end
         if @elements.empty?
             raise StackUnderflowCrash.new
         end
         return @elements.pop
+    end
+
+    def size
+        @elements.size
     end
 
 end
