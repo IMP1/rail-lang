22cac88dc8b1f0d6f315fd6f1877de92bc114df5
IMP1 <IMP1@mail.com>
2019-11-19 10:06:37 +0000

    Move test into same depth as others

--- tests/std_lib/string_split.rb
+++ tests/std_lib/string_split.rb
@@ -3,17 +3,20 @@ require_relative '../test'
 Test.require do
 
     require_relative '../../runner'
-    
 
 end
 
 test_run = Test.run do
-    source = File.read("../../../examples/string-lib.rl")
+    source = '
+$ \'main\'
+ \
+  ---[hello world]-{string-length}-o-#
+    '
     runner = Runner.new(source, "string-lib.rl")
     runner.run
 end
 
 test_run.ensure do |result|
     assert(result.success)
-    assert(result.output.chomp == "1")
+    assert(result.output.chomp == "11")
 end
