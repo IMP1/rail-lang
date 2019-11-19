641244b2362a70ad57d84d2ce18a26ba841c83e4
IMP1 <IMP1@mail.com>
2019-11-19 11:11:44 +0000

    Comment out broken code

--- tests/std_lib/string_split.rb
+++ tests/std_lib/string_split.rb
@@ -6,17 +6,33 @@ Test.require do
 
 end
 
-test_run = Test.run do
+test_run_1 = Test.run do
     source = '
 $ \'main\'
  \
-  ---[hello world]-{string-length}-o-#
+  ---[hello world]-{string-chars}-o-#
     '
     runner = Runner.new(source, "string-lib.rl")
     runner.run
 end
 
-test_run.ensure do |result|
+test_run_1.ensure do |result|
     assert(result.success)
-    assert(result.output.chomp == "11")
+    assert(result.output.chomp == "hello world".split("").inspect)
 end
+
+
+# test_run_2 = Test.run do
+#     source = '
+# $ \'main\'
+#  \
+#   ---[a]-{string-chars}-{list-splat}-o-#
+#     '
+#     runner = Runner.new(source, "string-lib.rl")
+#     runner.run
+# end
+
+# test_run_2.ensure do |result|
+#     assert(result.success)
+#     puts result.output
+# end
