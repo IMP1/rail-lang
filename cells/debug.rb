05343ce1bd10db15b1f939e98b152b4a8c324c9d
IMP1 <IMP1@mail.com>
2019-11-19 11:12:03 +0000

    Also print variables when debugging

--- cells/debug.rb
+++ cells/debug.rb
@@ -2,5 +2,7 @@ Cell.create("Debug", '`') do |cell, world, train, stack, env|
     $debug = false if $debug.nil?
     $debug = !$debug
     puts "DEBUG " + ($debug ? "ON" : "OFF")
-    p stack if $debug
+    if $debug
+        p env
+    end
 end
