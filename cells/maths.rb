13c0afde1dbc4b22b047ecf24a5b4b670935ff12
IMP1 <IMP1@mail.com>
2020-02-27 15:10:05 +0000

    Add a 'random number' cell

--- cells/maths.rb
+++ cells/maths.rb
@@ -69,3 +69,17 @@ end
         stack.push(num.to_s)
     end
 end
+
+# This isn't in the Rail Specifications, but there's no way to get a random number otherwise.
+Cell.create("Random", 'k') do |cell, world, train, stack, env|
+    b = stack.pop
+    a = stack.pop
+    unless Type.numeric?(a)
+        raise TypeMismatchCrash.new(Type::NUMBER, a)
+    end
+    unless Type.numeric?(b)
+        raise TypeMismatchCrash.new(Type::NUMBER, b)
+    end
+    c = b.to_i + rand(a.to_i - b.to_i)
+    stack.push(c.to_s)
+end
